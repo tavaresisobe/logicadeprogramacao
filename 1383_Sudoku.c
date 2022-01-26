814dd678b8fd7e877166982cdfa553cfe5996c25
Gustavo Henrique Tavares Isobe <91920423+tavaresisobe@users.noreply.github.com>
2022-01-26 12:30:40 -0300

    Update 1383_Sudoku.c

--- 1383_Sudoku.c
+++ 1383_Sudoku.c
@@ -1,9 +1,13 @@
+/*Gustavo Henrique Tavares Isobe
+  Exercicio número 1383 URI online: 
+  https://www.beecrowd.com.br/judge/pt/problems/view/1383 
+  Data: 26/01/2022 */
 #include <stdio.h>
 #include <stdlib.h>
 
 void sudoku (int matriz[9][9], int nmat)
 {
-    int i, j, sum, jogadores, vetor[10], count = 0, linhaGrade, colunaGrade ;
+    int i, j, k, jogadores, vetor[10], count = 0 ;
  // printf ("%d\n", nmat) ;
     for (jogadores = 1 ; jogadores <= nmat ; jogadores++)
     {
@@ -12,7 +16,7 @@ void sudoku (int matriz[9][9], int nmat)
          for ( j = 0 ; j < 9 ; j++)
          {
              scanf ("%d", &matriz[i][j]) ;
-             //condição para checar se elementos digitados estão entre [1...9]
+		    //condição para checar se elementos digitados estão entre [1...9]
                     if (matriz [i][j] < 1 || matriz[i][j] > 9)
                     {
                         count = 1 ;
@@ -20,7 +24,6 @@ void sudoku (int matriz[9][9], int nmat)
          }
       }
     //checar linhas:
-    
     for(i = 0 ; i < 9 && count != 1 ; i++)//se count for 1 temos que interromper o laço
     {
         for(j = 0 ; j < 10 ; j++)
@@ -38,7 +41,7 @@ void sudoku (int matriz[9][9], int nmat)
                 }
         }
     }
-    //checa colunas:
+    //checar colunas:
     for(j = 0 ; j < 9 && count != 1 ; j++)//se count for 1 temos que interromper o laço
     {
         for(i = 0 ; i < 10 ; i++)
@@ -57,31 +60,29 @@ void sudoku (int matriz[9][9], int nmat)
         }
     }
     //checar quadrados:
-    for(i = 2 ; i < 9 && !count ; i += 3)
+    for(i = 2 ; i < 9 && count != 1  ; i += 3)
     {
-	      	memset(vetor, 0, sizeof(vetor)) ;
-	      	for(j = i - 2 ; j <= i && !count ; j++)
+       vetor[10] = 0 ;
+	      	for(j = i - 2 ; j <= i && count != 1 ; j++)
 	      	{
-	    		for(k = i - 2; k <= i && !count ; k++)
+	    		for(k = i - 2; k <= i && count != 1 ; k++)
 	    		{
 	      			if(vetor[matriz[j][k]])
-	        			count = 1 ;
-	      			else
 	        			vetor[matriz[j][k]] = 1 ;
+	      			else
+	        		    count = 1 ;
 	    		}
 	      	}
-	 }
+     }
         printf("Instancia %d\n", jogadores) ;
-        if (count == 0)
-        {
+         if (count == 0)
             printf("SIM\n\n") ;
-        }else
+         else
             printf ("NAO\n\n") ;
     }
-    return ;
 }
-
 int main ()
+
 {
     int nmatrix ;
     scanf ("%d", &nmatrix) ;
@@ -89,3 +90,4 @@ int main ()
     sudoku (mat, nmatrix) ;
     return 0 ;
 }
+        
