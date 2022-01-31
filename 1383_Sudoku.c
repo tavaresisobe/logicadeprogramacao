4adfdcf04f188a3fad76435f54fd80e34badaeb5
Gustavo Henrique Tavares Isobe <91920423+tavaresisobe@users.noreply.github.com>
2022-01-31 18:33:57 -0300

    Update 1383_Sudoku.c

--- 1383_Sudoku.c
+++ 1383_Sudoku.c
@@ -1,76 +1,61 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void sudoku (int mat[9][9], int nmat)
+void sudoku (int nmat)
 {
-    int instancias ;
-    int i, j, l[10] = {0}, c[10] = {0}, q[10] = {0}, m, b, k, count = 0 ; 
-    for (instancias = 1 ; instancias <= nmat ; instancias++)
-    {
-        for (i = 0 ; i < 9 ; i++)
-        {
-            for (j = 0 ; j < 9 ; j++)
-            {
+    int instancias;
+    int mat[9][9];
+    int i, j;
+    int l[10], c[10], q[10];
+    int m, b, k, count = 0;
+        
+    
+    for (instancias = 1 ; instancias <= nmat ; instancias++){
+        count = 0;
+        
+        //le
+        for (i = 0 ; i < 9 ; i++){
+            for (j = 0 ; j < 9 ; j++){
                 scanf ("%d", &mat[i][j]) ;
             }
         }
-    //checando linhas:
-    for (i = 0 ; i < 9 ; i++)
-    {
-        for (j = 0 ; j < 9 ; j++)
-        {
-            l[mat[i][j]]++ ;
-        }
-        for (k = 1 ; k <= 9 ; k++)
-        if (l[k] != 1)
-        {
-            count = 1 ;
-        }
+        
+        //checando linhas:
+        for (i = 0 ; i < 9 ; i++){
+            for (k = 0 ; k <= 9 ; k++) l[k] = 0;
+            for (j = 0 ; j < 9 ; j++)
+                l[mat[i][j]]++ ;
             for (k = 1 ; k <= 9 ; k++)
-            {
-                l[k] = 0 ;
-            }
-    }
-    //checando colunas:
-    for (i = 0 ; i < 9 ; i++)
-    {
-        for (j = 0 ; j < 9 ; j++)
-        {
-            c[mat[j][i]]++ ;
+                if (l[k] != 1) count = 1 ;
         }
-        for (k = 1 ; k <= 9 ; k++)
-        if (c[k] != 1)
-        {
-            count = 1 ;
+        
+        //checando colunas:
+        for (i = 0 ; i < 9 ; i++){
+            for (k = 0 ; k <= 9 ; k++) c[k] = 0;
+            for (j = 0 ; j < 9 ; j++)
+                c[mat[j][i]]++ ;
+            for (k = 1 ; k <= 9 ; k++) 
+                if (c[k] != 1) count = 1;
         }
-            for (k = 1 ; k <= 9 ; k++)
-            {
-                c[k] = 0 ;
-            }
-    }
-    //checando quadrados:
-    for (m = 0 ; m < 9 ; m += 3)
-    {
-        for ( b = 0 ; b < 9 ; b += 3)
-        {
-            for (i = m ; i < m+3 ; i++)
-            {
-                for (j = b ; j < b+3 ; j++)
-                {
-                    q[mat[i][j]]++ ;
+        
+        //checando quadrados:
+        for (m = 0 ; m < 9 ; m += 3){
+            for ( b = 0 ; b < 9 ; b += 3){
+                //clean
+                for (k = 0 ; k <= 9 ; k++) q[k] = 0;
+                
+                for (i = m ; i < m+3 ; i++){
+                    for (j = b ; j < b+3 ; j++){
+                        q[mat[i][j]]++;
+                    }
                 }
-            }
-            for (k = 1 ; k <= 9 ; k++)
-                if (q[k] != 1)
-                {
-                   count = 1 ;
-                }
-            for (k = 1 ; k <= 9 ; k++)
-            {
-                q[k] = 0 ;
+                
+                for (k = 1 ; k <= 9 ; k++)
+                    if (q[k] != 1) count = 1 ;
             }
         }
-    }
-    printf ("Instancia %d\n", instancias) ;
+        
+        printf ("Instancia %d\n", instancias) ;
         if (count == 0)
             printf ("SIM\n\n") ;
         else
@@ -80,8 +65,8 @@ void sudoku (int mat[9][9], int nmat)
 
 int main ()
 {
-    int sudok[9][9], nmat ;
+    int nmat ;
     scanf ("%d", &nmat) ;
-    sudoku (sudok, nmat) ;
+    sudoku (nmat) ;
     return 0 ;
 }
