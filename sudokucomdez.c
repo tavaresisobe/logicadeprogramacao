b4d7e6816335a48677f7b3e12e25625be50ef064
Gustavo Henrique Tavares Isobe <91920423+tavaresisobe@users.noreply.github.com>
2022-02-01 18:59:53 -0300

    Update sudokucomdez.c

--- sudokucomdez.c
+++ sudokucomdez.c
@@ -8,15 +8,18 @@ void sudoku (int nmat)
     int i, j;
     int l[11], c[11], q[11];
     int m, b, k, count = 0;
-
-    for (instancias = 1 ; instancias <= nmat ; instancias++)
-       count = 0 ;
+        
+    
+    for (instancias = 1 ; instancias <= nmat ; instancias++){
+        count = 0;
+        
         //le
         for (i = 0 ; i < 9 ; i++){
             for (j = 0 ; j < 9 ; j++){
                 scanf ("%d", &mat[i][j]) ;
             }
         }
+        
         //checando linhas:
         for (i = 0 ; i < 9 && count != 1 ; i++){
             for (k = 0 ; k <= 10 ; k++) l[k] = 0;
@@ -25,6 +28,7 @@ void sudoku (int nmat)
             for (k = 1 ; k <= 10 & count != 1; k++)
                 if (l[k] > 1) count = 1 ;
         }
+        
         //checando colunas:
         for (i = 0 ; i < 9 && count != 1; i++){
             for (k = 0 ; k <= 10 ; k++) c[k] = 0;
@@ -32,22 +36,25 @@ void sudoku (int nmat)
                 c[mat[j][i]]++ ;
             for (k = 1 ; k <= 10 && count != 1 ; k++) 
                 if (c[k] > 1) count = 1;
-        }    
+        }
+        
         //checando quadrados:
         for (m = 0 ; m < 9 && count != 1 ; m += 3){
             for ( b = 0 ; b < 9 && count != 1 ; b += 3){
                 //clean
-                for (k = 0 ; k <= 10 ; k++) q[k] = 0;           
+                for (k = 0 ; k <= 10 ; k++) q[k] = 0;
+                
                 for (i = m ; i < m+3 && count != 1 ; i++){
                     for (j = b ; j < b+3 && count != 1 ; j++){
                         q[mat[i][j]]++;
                     }
+                }
                 
                 for (k = 1 ; k <= 10 ; k++)
                     if (q[k] > 1) count = 1 ;
-                }
             }
-        }        
+        }
+        
         printf ("Instancia %d\n", instancias) ;
         if (count == 0)
             printf ("SIM\n\n") ;
