2535a5eebb5e64996be7d46a78be82caa98d5758
Gustavo Henrique Tavares Isobe <91920423+tavaresisobe@users.noreply.github.com>
2022-07-26 22:38:37 -0300

    Update 1042_SortSimples.c

--- 1042_SortSimples.c
+++ 1042_SortSimples.c
@@ -1,44 +1,43 @@
-/*Leia 3 valores inteiros e ordene-os em ordem crescente. No 
-final, mostre os valores em ordem crescente, uma linha em branco
-e em seguida, os valores na sequência como foram lidos.*/
-
-#include <stdio.h>
-
-int main ()
-{
-    int a, b, c;
-    scanf("%d %d %d", &a, &b, &c);
-    
-    if (a < b && a < c) 
-    {
-        if (b < c) 
-        {
-            printf("%d\n%d\n%d\n", a, b, c);
-        }
-        else{
-                printf("%d\n%d\n%d\n", a, c, b);
-            }
-    }
-    if (b < a && b < c) 
-    {
-        if (a < c) 
-        {
-            printf("%d\n%d\n%d\n", b, a, c);
-        }
-        else{
-                printf("%d\n%d\n%d\n", b, c, a);
-            }
-    }    
-    if (c < a && c < b)
-    {
-        if (a < b)
-        {
-            printf("%d\n%d\n%d\n", c, a, b);
-        }
-        else{
-                printf("%d\n%d\n%d\n", c, b, a);
-            }
-    }
-    printf("\n%d\n%d\n%d\n", a, b, c);
-    return 0 ;
+/*Leia 3 valores inteiros e ordene-os em ordem crescente. No 
+final, mostre os valores em ordem crescente, uma linha em branco
+e em seguida, os valores na sequência como foram lidos.*/
+
+#include <stdio.h>
+
+int main ()
+{
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    if (a < b && a < c) 
+    {
+        if (b < c) 
+        {
+            printf("%d\n%d\n%d\n", a, b, c);
+        }
+        else{
+                printf("%d\n%d\n%d\n", a, c, b);
+            }
+    }
+    if (b < a && b < c) 
+    {
+        if (a < c) 
+        {
+            printf("%d\n%d\n%d\n", b, a, c);
+        }
+        else{
+                printf("%d\n%d\n%d\n", b, c, a);
+            }
+    }    
+    if (c < a && c < b)
+    {
+        if (a < b)
+        {
+            printf("%d\n%d\n%d\n", c, a, b);
+        }
+        else{
+                printf("%d\n%d\n%d\n", c, b, a);
+            }
+    }
+    printf("\n%d\n%d\n%d\n", a, b, c);
+    return 0 ;
 }
